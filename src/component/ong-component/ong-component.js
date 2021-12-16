03b388a1161d67b5adf03b8a73bf70748a3bc289
Quentin Leblanc <quentin.leblanc@hes-so.ch>
2021-12-17 00:40:27 +0100

    finihed component gov

--- src/component/ong-component/ong-component.js
+++ src/component/ong-component/ong-component.js
@@ -1,25 +1,72 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {queryOng} from "../../services/queryServices";
+import {queryOng, queryYears} from "../../services/queryServices";
 import BubbleChart from '@weknow/react-bubble-chart-d3';
-import ReactHTMLDatalist from "react-html-datalist";
+import {
+    Divider,
+    FormControl, FormControlLabel,
+    Grid,
+    InputLabel,
+    makeStyles,
+    MenuItem,
+    Paper, Radio, RadioGroup,
+    Select,
+    Typography
+} from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        display: 'block',
+        marginTop: theme.spacing(2),
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+}));
+
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height} = window;
+    return {
+        width,
+        height
+    };
+}
+
 
 function OngComponent() {
 
     const [data, setData] = useState([]);
     const [dataBubbleChart, setDataBubbleChart] = useState([]);
     const [year, setYear] = useState(2017);
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    const [open, setOpen] = React.useState(false);
+    const [years, setYears] = useState([]);
+    const [selectedValueFilter, setSelectedValueFilter] = useState('<');
+    const [selectedValuePercent, setSelectedValuePercent] = useState(10);
+
+    const classes = useStyles();
 
     useEffect(() => {
-        queryOng(year).then(data => {
-            // setYear(yearsToArray(data));
-            // setBroadbandData(broadbandToArray(data));
-            // setMobileData(mobileToArray(data));
-            // setShareData(shareToArray(data));
-            console.log("data receive : " + data[0]["country"]);
-            setData(data);
-            transformDataToBubbleChartData(data);
+        queryOng(year).then(d => {
+            console.log("data receive : " + d[0]["country"]);
+            setData(d);
         });
     }, [year]);
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        queryYears().then( ys => {
+            setYears(ys.map(y => +y));
+        });
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
 
     const bubbleClick = (label) => {
         console.log("Custom bubble click func")
@@ -33,129 +80,170 @@ function OngComponent() {
 
         data = data.filter(item => item.country !== 'total' && item.country !== "World");
         setDataBubbleChart(data.map(item => {
-            return {label: item["country"], value: item["numberUser"]}
+            return {label: item["country"], value: item["numberUser"].toFixed(2)}
         }));
     }
 
     useEffect(() => {
-        if (dataBubbleChart.length > 0) {
+        if (dataBubbleChart !== undefined && dataBubbleChart.length > 0) {
             console.log("data for BubbleChart = " + dataBubbleChart[0].label + " value " +dataBubbleChart[0].value);
         }
 
     }, [dataBubbleChart])
 
-    /*const handleChange = e => {
-        console.log(e.target.value);
-        setYear(e.target.value);
-    }*/
+    const handleChange = (event) => {
+        setYear(+event.target.value);
+    };
 
-    const handleButtonClick = () => {
-        console.log(input.current.value);
-        setYear(input.current.value);
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    useEffect(() => {
+        console.log(selectedValuePercent);
+        if (selectedValueFilter === '<') {
+            transformDataToBubbleChartData(data.filter(d => +d["numberUser"] < selectedValuePercent));
+        } else {
+            transformDataToBubbleChartData(data.filter(d => +d["numberUser"] > selectedValuePercent));
+        }
+    }, [data, selectedValueFilter, selectedValuePercent]);
+
+    const handleRadioChange = (event) => {
+        setSelectedValueFilter(event.target.value);
     }
 
-    const input = useRef(null);
+    const handleRadioChangePercent = (event) => {
+        setSelectedValuePercent(+event.target.value);
+    }
 
     return (
-        <>
-            {/*<form >
-                <label> Choose your years: </label>
-                <input list="years" type="text" onClick={handleChange} />
-                <datalist id="years">
-                    <option value="2003"/>
-                    <option value="2004"/>
-                    <option value="2005"/>
-                    <option value="2006"/>
-                    <option value="2007"/>
-                    <option value="2008"/>
-                    <option value="2009"/>
-                    <option value="2010"/>
-                    <option value="2011"/>
-                    <option value="2012"/>
-                    <option value="2013"/>
-                    <option value="2014"/>
-                    <option value="2015"/>
-                    <option value="2016"/>
-                    <option value="2017"/>
-                </datalist>
-            </form>*/}
-            <p>Select the year :
-                <input type="text" ref={input} value={year}/>
-                <button onClick={handleButtonClick}>submit</button>
-            </p>
-            {/*<ReactHTMLDatalist name="years-list"
-                               onChange={handleChange}
-                               options={[
-                                   {text: "2003", value: "2003"},
-                                   {text: "2004", value: "2004"},
-                                   {text: "2005", value: "2005"},
-                                   {text: "2006", value: "2006"},
-                                   {text: "2007", value: "2007"},
-                                   {text: "2008", value: "2008"},
-                                   {text: "2009", value: "2009"},
-                                   {text: "2010", value: "2010"},
-                                   {text: "2011", value: "2011"},
-                                   {text: "2012", value: "2012"},
-                                   {text: "2013", value: "2013"},
-                                   {text: "2014", value: "2014"},
-                                   {text: "2015", value: "2015"},
-                                   {text: "2016", value: "2016"},
-                                   {text: "2017", value: "2017"}
-                               ]}
-            />*/}
-            <BubbleChart
-                graph={{
-                    zoom: 1.1,
-                    offsetX: -0.05,
-                    offsetY: -0.01,
-                }}
-                width={1000}
-                height={800}
-                padding={0} // optional value, number that set the padding between bubbles
-                showLegend={true} // optional value, pass false to disable the legend.
-                legendPercentage={20} // number that represent the % of with that legend going to use.
-                legendFont={{
-                    family: 'Arial',
-                    size: 12,
-                    color: '#000',
-                    weight: 'bold',
-                }}
-                valueFont={{
-                    family: 'Arial',
-                    size: 12,
-                    color: '#fff',
-                    weight: 'bold',
-                }}
-                labelFont={{
-                    family: 'Arial',
-                    size: 16,
-                    color: '#fff',
-                    weight: 'bold',
-                }}
-                //Custom bubble/legend click functions such as searching using the label, redirecting to other page
-                bubbleClickFunc={bubbleClick}
-                legendClickFun={legendClick}
-                /*data={[
-                    {label: 'CRM', value: 1},
-                    {label: 'API', value: 1},
-                    {label: 'Data', value: 1},
-                    {label: 'Commerce', value: 1},
-                    {label: 'AI', value: 3},
-                    {label: 'Management', value: 5},
-                    {label: 'Testing', value: 6},
-                    {label: 'Mobile', value: 9},
-                    {label: 'Conversion', value: 9},
-                    {label: 'Misc', value: 21},
-                    {label: 'Databases', value: 22},
-                    {label: 'DevOps', value: 22},
-                    {label: 'Javascript', value: 23},
-                    {label: 'Languages / Frameworks', value: 25},
-                    {label: 'Front End', value: 26},
-                    {label: 'Content', value: 26},
-                ]}*/
-                data={dataBubbleChart}
-            />
-        </>
+        <div style={{ padding: 20 }}>
+            <Grid container justifyContent="center" alignItems="center">
+                <Grid item xs={7}>
+                    <BubbleChart
+                        graph={{
+                            zoom: 0.95,
+                            offsetX: 0.005,
+                            offsetY: -0.01,
+                        }}
+                        width={windowDimensions.width/2}
+                        height={windowDimensions.height}
+                        padding={1} // optional value, number that set the padding between bubbles
+                        showLegend={true} // optional value, pass false to disable the legend.
+                        legendPercentage={10} // number that represent the % of with that legend going to use.
+                        legendFont={{
+                            family: 'Arial',
+                            size: 12,
+                            color: '#000',
+                            weight: 'bold',
+                        }}
+                        valueFont={{
+                            family: 'Arial',
+                            size: 12,
+                            color: '#fff',
+                            weight: 'bold',
+                        }}
+                        labelFont={{
+                            family: 'Arial',
+                            size: 16,
+                            color: '#fff',
+                            weight: 'bold',
+                            'stroke-width': 2,
+                        }}
+                        //Custom bubble/legend click functions such as searching using the label, redirecting to other page
+                        bubbleClickFunc={bubbleClick}
+                        legendClickFun={legendClick}
+                        data={dataBubbleChart}
+                    />
+                </Grid>
+                <Grid item xs alignItems="center">
+                    <Paper className="mt-4">
+                        <Typography id="global-params" component='h5' variant='h5' style={{textAlign: "center"}} gutterBottom>Chose the year</Typography>
+                        <Divider />
+                        <div className="ml-4" align="center">
+                            <FormControl className={classes.formControl}>
+                                <Select
+                                    native
+                                    labelId="demo-controlled-open-select-label"
+                                    id="demo-controlled-open-select"
+                                    open={open}
+                                    onClose={handleClose}
+                                    onOpen={handleOpen}
+                                    value={year}
+                                    onChange={handleChange}
+                                >
+                                    {years.map(y => (
+                                        <option value={y}>{y}</option>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </div>
+                        <Divider />
+                        <Typography id="global-params" component='h5' variant='h5' style={{textAlign: "center"}} gutterBottom>Filter results over or under the value of percentage below</Typography>
+                        <Divider />
+                        <div className="ml-4">
+                            <div className="ml-4" align="center">
+                                <FormControl component="fieldset">
+                                    <RadioGroup row defaultValue="<">
+                                        <FormControlLabel
+                                            value="<"
+                                            control={<Radio
+                                                        checked={selectedValueFilter === '<'}
+                                                        onChange={handleRadioChange}
+                                                        color="default"
+                                                        name="radio-button-demo"
+
+                                                        inputProps={{ 'aria-label': 'D' }}/>}
+                                            label="under (x < %)"
+                                            labelPlacement="start"/>
+
+                                        <FormControlLabel
+                                            control={<Radio
+                                                checked={selectedValueFilter === '>'}
+                                                onChange={handleRadioChange}
+                                                value=">"
+                                                color="default"
+                                                name="radio-button-demo"
+                                                inputProps={{ 'aria-label': 'D' }}/>}
+                                            label="over (x > %)"
+                                            labelPlacement="end"/>
+
+                                    </RadioGroup>
+                                </FormControl>
+                                <br/>
+                                <FormControl component="fieldset">
+                                    <RadioGroup row defaultValue={selectedValuePercent}>
+                                        {[1, 5, 10, 20, 40].map(k => {
+                                            return <FormControlLabel
+                                                control={<Radio
+                                                    checked={selectedValuePercent === k}
+                                                    onChange={handleRadioChangePercent}
+                                                    value={k}
+                                                    color="default"
+                                                    name="radio-button-demo"
+                                                    inputProps={{ 'aria-label': 'D' }}/>}
+                                                label={k}
+                                                labelPlacement="start"/>
+                                        })}
+                                    </RadioGroup>
+                                </FormControl>
+                            </div>
+                        </div>
+                        <Divider />
+                        <Typography id="global-params" component='h5' variant='h5' style={{textAlign: "center"}} gutterBottom>Statistics for year <b>{year}</b></Typography>
+                        <Divider />
+                        <div style={{paddingLeft: 20, paddingRight: 20, paddingBottom:10}}>
+                            <p> <b>{dataBubbleChart.length}</b> {dataBubbleChart.length === 1 ? 'country is' :'countries are'} using each {selectedValueFilter === '<' ? 'under' : 'over'} <b>{selectedValuePercent}%</b> of the Internet traffic</p>
+                            <p> This represent <b>{(dataBubbleChart.length/data.length*100).toFixed(2)}%</b> of the countries in the world at this epoch</p>
+                        </div>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
     );
 }
 
--- src/services/queryServices.js
+++ src/services/queryServices.js
@@ -3,7 +3,6 @@ import {handleResponse} from "../helpers/helper";
 const BACKEND_URL = "http://127.0.0.1:5000";
 
 export async function queryGov(country) {
-    console.log(BACKEND_URL)
     const response = await fetch(`${BACKEND_URL}/gov/${country}`, {
         method: 'GET',
         mode: 'cors',
@@ -14,8 +13,17 @@ export async function queryGov(country) {
 }
 
 export async function queryOng(year) {
-    console.log(BACKEND_URL)
     const response = await fetch(`${BACKEND_URL}/ong/${year}`, {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-store',
+        headers: {'Content-Type':  'application/json'}
+    });
+    return handleResponse(response);
+}
+
+export async function queryYears() {
+    const response = await fetch(`${BACKEND_URL}/ong/getYears`, {
         method: 'GET',
         mode: 'cors',
         cache: 'no-store',
